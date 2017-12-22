a97abff5695adbbad37470da32ae593cde7243bb
Jacob Dufault <jacobdufault@gmail.com>
2017-12-22 08:26:27 -0800

    Update tests

--- tests/namespaces/anonymous_function.cc
+++ tests/namespaces/anonymous_function.cc
@@ -13,8 +13,8 @@ OUTPUT:
       "is_operator": false,
       "usr": "c:anonymous_function.cc@aN@F@foo#",
       "short_name": "foo",
-      "detailed_name": "void ::foo()",
-      "hover": "void ::foo()",
+      "detailed_name": "void (anon)::foo()",
+      "hover": "void (anon)::foo()",
       "declarations": [{
           "spelling": "2:6-2:9",
           "extent": "2:1-2:11",
