28bdc0cf026c35371e3938cdc118c407090e8499
Chao Shen <chao.shen@uisee.com>
2018-02-02 08:48:37 +0800

    Check if using libstdc++ before include <string_view>.

--- third_party/string_view.h
+++ third_party/string_view.h
@@ -1,4 +1,5 @@
-#if __cplusplus >= 201703L || defined(__has_include) && __has_include(<string_view>)
+#include <cstddef> // to check __GLIBCXX__
+#if __cplusplus >= 201703L || (defined(__has_include) && __has_include(<string_view>) && !defined(__GLIBCXX__))
 #include <string_view>
 #else
 #define STX_NAMESPACE_NAME std
