57a4cf3265e1e38792959ee3d878f902427a46eb
Fangrui Song <i@maskray.me>
2018-02-01 23:27:39 -0800

    Fix string_view.h

--- third_party/string_view.h
+++ third_party/string_view.h
@@ -1,7 +1,3 @@
-#include <cstddef> // to check __GLIBCXX__
-#if __cplusplus >= 201703L || (defined(__has_include) && __has_include(<string_view>) && !defined(__GLIBCXX__))
-#include <string_view>
-#else
+#pragma once
 #define STX_NAMESPACE_NAME std
 #include "string_view.hpp"
-#endif
