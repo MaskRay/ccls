d7e0722ca6c100bab0eb0b931e7ae7d4a6d4af49
Fangrui Song <i@maskray.me>
2018-03-17 23:41:04 -0700

    Use clang+llvm-6.0.0-amd64-unknown-freebsd-10.tar.xz
    before 6.0.0 it was named freebsd10

--- cmake/DefaultCMakeBuildType.cmake
+++ cmake/DefaultCMakeBuildType.cmake
@@ -9,10 +9,10 @@ endif()
 if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
   message(STATUS "Setting build type to '${DEFAULT_CMAKE_BUILD_TYPE}' as none \
 was specified.")
-  set(CMAKE_BUILD_TYPE ${DEFAULT_CMAKE_BUILD_TYPE} 
+  set(CMAKE_BUILD_TYPE ${DEFAULT_CMAKE_BUILD_TYPE}
       CACHE STRING "Choose the type of build." FORCE)
-  
+
   # Set the possible values of build type for cmake-gui
-  set_property(CACHE CMAKE_BUILD_TYPE 
+  set_property(CACHE CMAKE_BUILD_TYPE
                PROPERTY STRINGS Debug Release MinSizeRel RelWithDebInfo)
 endif()
