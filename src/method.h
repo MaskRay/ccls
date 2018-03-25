b68b063abde1e818718112e0c94c6d4b7fd9550e
Maxim Kot <batbko@gmail.com>
2018-03-25 18:21:42 +0000

    UB fixed. Forgotten return was removed.

--- src/method.h
+++ src/method.h
@@ -5,7 +5,7 @@
 
 #include <string>
 
-using MethodType = std::string;
+using MethodType = const char*;
 extern MethodType kMethodType_Unknown;
 extern MethodType kMethodType_Exit;
 extern MethodType kMethodType_TextDocumentPublishDiagnostics;
