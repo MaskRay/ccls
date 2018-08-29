74790e2421a67a74fade74f99deb9feaaef81eae
firstlove <37442588+FirstLoveLife@users.noreply.github.com>
2018-08-29 23:33:20 +0800

    fix clang_tu.h && clang_complete.cc (#61)

--- src/clang_complete.cc
+++ src/clang_complete.cc
@@ -515,7 +515,7 @@ BuildCompilerInstance(CompletionSession &session,
         Preamble->Preamble.OverridePreamble(*CI, session.FS,
                                             Bufs.back().get());
 #else
-        Preamble->Preamble.AddImplicitPreamble(*CI, session->FS,
+        Preamble->Preamble.AddImplicitPreamble(*CI, session.FS,
                                                Bufs.back().get());
 #endif
       } else {
