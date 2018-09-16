c446090657d4633e50a323a6af75184b4e2712bf
Fangrui Song <i@maskray.me>
2018-09-16 11:07:15 -0700

    Add clang.pathMappings to reuse cache files with differect source paths
    Use `>` as the separator.

--- src/config.cc
+++ src/config.cc
@@ -5,3 +5,16 @@
 
 Config *g_config;
 thread_local int g_thread_id;
+
+namespace ccls {
+void DoPathMapping(std::string &arg) {
+  for (const std::string &mapping : g_config->clang.pathMappings) {
+    auto sep = mapping.find('>');
+    if (sep != std::string::npos) {
+      auto p = arg.find(mapping.substr(0, sep));
+      if (p != std::string::npos)
+        arg.replace(p, sep, mapping.substr(sep + 1));
+    }
+  }
+}
+}
