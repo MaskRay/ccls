90357034df99c6dd181f9ee63f379ff9c77ce249
Riatre Foo <foo@riat.re>
2018-10-14 07:58:08 +0800

    textDocument/signatureHelp: enable documentation

--- src/messages/textDocument_signatureHelp.cc
+++ src/messages/textDocument_signatureHelp.cc
@@ -91,10 +91,8 @@ public:
 
       const char *ret_type = nullptr;
       SignatureInformation &ls_sig = ls_sighelp.signatures.emplace_back();
-#if LLVM_VERSION_MAJOR >= 8
       const RawComment *RC = getCompletionComment(S.getASTContext(), Cand.getFunction());
       ls_sig.documentation = RC ? RC->getBriefText(S.getASTContext()) : "";
-#endif
       for (const auto &Chunk : *CCS)
         switch (Chunk.Kind) {
         case CodeCompletionString::CK_ResultType:
@@ -171,7 +169,7 @@ void MessageHandler::textDocument_signatureHelp(
   CodeCompleteOptions CCOpts;
   CCOpts.IncludeGlobals = false;
   CCOpts.IncludeMacros = false;
-  CCOpts.IncludeBriefComments = false;
+  CCOpts.IncludeBriefComments = true;
   if (cache.IsCacheValid(path, begin_pos)) {
     SignatureHelpConsumer Consumer(CCOpts, true);
     cache.WithLock([&]() { Consumer.ls_sighelp = cache.result; });
