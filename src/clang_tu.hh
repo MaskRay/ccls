18b47d3f5c31d30d5dfef11e057697538b105e05
Fangrui Song <i@maskray.me>
2018-10-18 22:11:43 -0700

    Improve extent of definition/declaration; uniquify typeDefinition

--- src/clang_tu.hh
+++ src/clang_tu.hh
@@ -32,6 +32,11 @@ Range FromTokenRange(const clang::SourceManager &SM,
                      const clang::LangOptions &LangOpts, clang::SourceRange R,
                      llvm::sys::fs::UniqueID *UniqueID = nullptr);
 
+Range FromTokenRangeDefaulted(const clang::SourceManager &SM,
+                              const clang::LangOptions &Lang,
+                              clang::SourceRange R, const clang::FileEntry *FE,
+                              Range range);
+
 std::unique_ptr<clang::CompilerInvocation>
 BuildCompilerInvocation(std::vector<const char *> args,
                         llvm::IntrusiveRefCntPtr<llvm::vfs::FileSystem> VFS);
