af4d81d387f8e50416fa7e57da560b1e1124ef79
Fangrui Song <i@maskray.me>
2018-11-23 10:08:44 -0800

    Refactor ReplyOnce; error if InitializeParams.rootUri is null

--- src/messages/initialize.cc
+++ src/messages/initialize.cc
@@ -292,10 +292,7 @@ void Initialize(MessageHandler *m, InitializeParam &param, ReplyOnce &reply) {
 
   // Send initialization before starting indexers, so we don't send a
   // status update too early.
-  {
-    InitializeResult result;
-    reply(result);
-  }
+  reply(InitializeResult{});
 
   // Set project root.
   EnsureEndsInSlash(project_path);
@@ -342,8 +339,10 @@ void Initialize(MessageHandler *m, InitializeParam &param, ReplyOnce &reply) {
 void MessageHandler::initialize(Reader &reader, ReplyOnce &reply) {
   InitializeParam param;
   Reflect(reader, param);
-  if (!param.rootUri)
+  if (!param.rootUri) {
+    reply.Error(ErrorCode::InvalidRequest, "expected rootUri");
     return;
+  }
   Initialize(this, param, reply);
 }
 
@@ -355,8 +354,7 @@ void StandaloneInitialize(MessageHandler &handler, const std::string &root) {
 }
 
 void MessageHandler::shutdown(EmptyParam &, ReplyOnce &reply) {
-  JsonNull result;
-  reply(result);
+  reply(JsonNull{});
 }
 
 void MessageHandler::exit(EmptyParam &) {
