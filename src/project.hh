e8cacf1efa388b1c7bd2f3a66cb63e034538b5c5
Fangrui Song <i@maskray.me>
2018-12-23 21:22:51 -0800

    Adjust FrontendOpts.Inputs[0] for inferred files

--- src/project.hh
+++ src/project.hh
@@ -68,7 +68,7 @@ struct Project {
 
   // Lookup the CompilationEntry for |filename|. If no entry was found this
   // will infer one based on existing project structure.
-  Entry FindEntry(const std::string &path, bool can_be_inferred);
+  Entry FindEntry(const std::string &path, bool can_redirect, bool must_exist);
 
   // If the client has overridden the flags, or specified them for a file
   // that is not in the compilation_database.json make sure those changes
