79d6c27ae154c74d23989d73bec1bd89f84c13a0
Fangrui Song <i@maskray.me>
2019-06-27 19:11:38 -0700

    project: support %cu directive and strip -M* options

--- src/lsp.hh
+++ src/lsp.hh
@@ -249,6 +249,13 @@ struct ShowMessageParam {
 // Used to identify the language at a file level. The ordering is important, as
 // a file previously identified as `C`, will be changed to `Cpp` if it
 // encounters a c++ declaration.
-enum class LanguageId { Unknown = -1, C = 0, Cpp = 1, ObjC = 2, ObjCpp = 3 };
+enum class LanguageId {
+  Unknown = -1,
+  C = 0,
+  Cpp = 1,
+  ObjC = 2,
+  ObjCpp = 3,
+  Cuda = 4,
+};
 
 } // namespace ccls
