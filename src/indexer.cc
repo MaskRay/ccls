17dc4a6ecc49ccc69870bdb574ae7e67a9b40662
Fangrui Song <i@maskray.me>
2019-07-30 21:18:34 -0700

    Construct SourceManager with UserFilesAreVolatile
    Fix #235. Close #453
    
    In clang, it may be worth generalizing UserFilesAreVolatile (introduced in rC160074) to FilesAreVolatile.

--- src/indexer.cc
+++ src/indexer.cc
@@ -1255,6 +1255,8 @@ Index(SemaManager *manager, WorkingFiles *wfiles, VFS *vfs,
   Clang->setVirtualFileSystem(FS);
   Clang->createFileManager();
 #endif
+  Clang->setSourceManager(new SourceManager(Clang->getDiagnostics(),
+                                            Clang->getFileManager(), true));
 
   IndexParam param(*vfs, no_linkage);
   auto DataConsumer = std::make_shared<IndexDataConsumer>(param);
