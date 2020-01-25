1066548dbada552a546f77f958445e7f1a7ea48b
Fangrui Song <i@maskray.me>
2020-01-25 09:57:16 -0800

    Adapt D73392 - split of llvm/Support/Allocator.h

--- src/serializer.cc
+++ src/serializer.cc
@@ -1,4 +1,4 @@
-// Copyright 2017-2018 ccls Authors
+// Copyright 2017-2020 ccls Authors
 // SPDX-License-Identifier: Apache-2.0
 
 #include "serializer.hh"
@@ -13,6 +13,7 @@
 
 #include <llvm/ADT/CachedHashString.h>
 #include <llvm/ADT/DenseSet.h>
+#include <llvm/Support/Allocator.h>
 
 #include <mutex>
 #include <stdexcept>
