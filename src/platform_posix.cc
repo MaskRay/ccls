f3fe1ca86b0cf7ff63a43733fed1a9ecbe892935
Chris Roberts <cpr420@gmail.com>
2022-01-22 17:27:06 -0700

    platform_posix: add support for Haiku

--- src/platform_posix.cc
+++ src/platform_posix.cc
@@ -1,7 +1,7 @@
 // Copyright 2017-2018 ccls Authors
 // SPDX-License-Identifier: Apache-2.0
 
-#if defined(__unix__) || defined(__APPLE__)
+#if defined(__unix__) || defined(__APPLE__) || defined(__HAIKU__)
 #include "platform.hh"
 
 #include "utils.hh"
